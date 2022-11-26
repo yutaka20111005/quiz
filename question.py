a968e432d4a56d22cfe97429dbfd811b8ba32f6d
yutaka20111005 <araki716y@gmail.com>
2022-11-26 16:06:34 +0900

    Update question.py

--- question.py
+++ question.py
@@ -1,5 +1,6 @@
 QUESTION = [
   "24の最後のSEASONは、SEASON何？ ",
   "24 Live Another Dayで、Kateが救出中、撃たれて亡くなったのは誰？",
-  "24 SEASON 8で、エージェント Callと共に敵と銃撃戦の最中、絶体絶命だったJackを間一髪のところで助けたのは誰？"
+  "24 SEASON 8で、エージェント Callと共に敵と銃撃戦の最中、絶体絶命だったJackを間一髪のところで助けたのは誰？",
+  "Terri Bowerが亡くなったのは、SEASON何？"
 ]
